f31aa185efba8bc08c4e6a5a0912b24ed758563e
Pat Metz <49944076+wuyafeiya@users.noreply.github.com>
2024-09-27 11:46:33 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,12 +1,20 @@
 const pageConfig = {
-  title: "网站监测",
-   links: [
-    { link: '', label: 'GitHub' },
-    { link: '', label: 'Email Me', highlight: true },
+  // Title for your status page
+  title: "lyc8503's Status Page",
+  // Links shown at the header of your status page, could set `highlight` to `true`
+  links: [
+    { link: 'https://github.com/lyc8503', label: 'GitHub' },
+    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
+    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
   ],
 }
+
 const workerConfig = {
-  kvWriteCooldownMinutes: 5,
+  // Write KV at most every 3 minutes unless the status changed
+  kvWriteCooldownMinutes: 3,
+  // Enable HTTP Basic auth for status page & API by uncommenting the line below, format `<USERNAME>:<PASSWORD>`
+  // passwordProtection: 'username:password',
+  // Define all your monitors here
   monitors: [
     {
       id: 'origin',
@@ -39,6 +47,20 @@ const workerConfig = {
       target: 'https://foxicloud0.com'
     },
   ],
+  notification: {
+    // [Optional] apprise API server URL
+    // if not specified, no notification will be sent
+    appriseApiServer: "https://apprise.example.com/notify",
+    // [Optional] recipient URL for apprise, refer to https://github.com/caronc/apprise
+    // if not specified, no notification will be sent
+    recipientUrl: "tgram://bottoken/ChatID",
+    // [Optional] timezone used in notification messages, default to "Etc/GMT"
+    timeZone: "Asia/Shanghai",
+    // [Optional] grace period in minutes before sending a notification
+    // notification will be sent only if the monitor is down for N continuous checks after the initial failure
+    // if not specified, notification will be sent immediately
+    gracePeriod: 5,
+  },
   callbacks: {
     onStatusChange: async (
       env: any,
@@ -46,7 +68,7 @@ const workerConfig = {
       isUp: boolean,
       timeIncidentStart: number,
       timeNow: number,
-      reason: string,
+      reason: string
     ) => {
       // This callback will be called when there's a status change for any monitor
       // Write any Typescript code here
@@ -59,7 +81,7 @@ const workerConfig = {
       monitor: any,
       timeIncidentStart: number,
       timeNow: number,
-      reason: string,
+      reason: string
     ) => {
       // This callback will be called EVERY 1 MINTUE if there's an on-going incident for any monitor
       // Write any Typescript code here
